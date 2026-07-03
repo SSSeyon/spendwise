28651ddeb93d1eeb33a281d06eec748ff34e3a48
SSSeyon <ssseyon@gmail.com>
2026-07-03 18:37:02 +0100

    Upload failure

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // SpendWise Service Worker
 // Cache-first for static assets, network-only for Firebase/Firestore
-const CACHE = 'spendwise-v7';
+const CACHE = 'spendwise-v8';
 
 const STATIC = [
   './',
